d34e60ac0327ecc4d35713bb83fa69f0503e81a1
thiago viel denadai <125681715+thidenadai@users.noreply.github.com>
2024-03-22 13:04:25 -0300

    att

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Button, TextInput, Modal } from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("");
+
+  changeTxt = (txtInput) => {
+    setName(`Seja bem vindo, ${txtInput}`);
+  };
+
+  useEffect(() => {
+    if (name.length === 26) {
+      alert("limite de caracteres");
+    }
+  }, [name]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.inputname}
+        placeholder="Digite seu nome"
+        onChangeText={changeTxt}
+      />
+      <Text style={styles.boavindanome}>{name}</Text>
     </View>
   );
 }
@@ -13,8 +29,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputname: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: 300,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "black",
+    margin: 10,
+  },
+  boavindanome: {
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: 30,
   },
 });
